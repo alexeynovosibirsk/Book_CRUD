f0f90c50f6a01eb9278f1adb007c8c360350285d
alex <nixoved@yandex.ru>
2021-05-27 11:00:19 +0700

    someImproves

--- src/main/java/com/example/demo/service/PersonService.java
+++ src/main/java/com/example/demo/service/PersonService.java
@@ -4,10 +4,9 @@ import com.example.demo.entity.Person;
 import com.example.demo.repository.PersonRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.List;
-import java.util.Optional;
+
 
 @Service
 public class PersonService {
@@ -17,16 +16,7 @@ public class PersonService {
 
     public List<Person> findAll() {return personRepository.findAll();}
 
-    public Person update(Person person) {
-        Person existingPerson = personRepository.findPersonById(person.getId());
-        existingPerson.setName(person.getName());
-        existingPerson.setSurname(person.getSurname());
-        existingPerson.setAge(person.getAge());
-        return personRepository.save(existingPerson);
-    }
-
     public Person findPersonByName(String name) {
-
         return personRepository.findPersonByName(name);
     }
 
@@ -34,7 +24,13 @@ public class PersonService {
         return personRepository.save(person);
     }
 
-
+    public Person update(Person person) {
+        Person existingPerson = personRepository.findPersonById(person.getId());
+        existingPerson.setName(person.getName());
+        existingPerson.setSurname(person.getSurname());
+        existingPerson.setAge(person.getAge());
+        return personRepository.save(existingPerson);
+    }
 
     public void delete(long id) {
         personRepository.deleteById(id);
